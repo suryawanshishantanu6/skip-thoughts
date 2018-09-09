e920efebdd1c52a0f9e4c0f6fa228041ec51f9a1
suryawanshishantanu6 <suryawanshishantanu6@gmail.com>
2018-09-09 20:30:23 +0530

    Update skipthoughts.py

--- skipthoughts.py
+++ skipthoughts.py
@@ -20,8 +20,8 @@ profile = False
 #-----------------------------------------------------------------------------#
 # Specify model and table locations here
 #-----------------------------------------------------------------------------#
-path_to_models = '/content'
-path_to_tables = '/content'
+path_to_models = '/content/'
+path_to_tables = '/content/'
 #-----------------------------------------------------------------------------#
 
 path_to_umodel = path_to_models + 'uni_skip.npz'
